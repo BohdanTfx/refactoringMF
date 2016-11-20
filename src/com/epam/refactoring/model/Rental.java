8a3f081c03be0851e41189109fcc81eec23abc05
BohdanTfx <noreply>
2016-11-20 12:54:57 +0200

    Extracting Frequent Renter Points

--- src/com/epam/refactoring/model/Rental.java
+++ src/com/epam/refactoring/model/Rental.java
@@ -17,6 +17,14 @@ class Rental {
         return _movie;
     }
 
+    public int getFrequentRenterPoints() {
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
+                && getDaysRented() > 1)
+            return 2;
+        else
+            return 1;
+    }
+
     public double getCharge() {
         double result = 0;
         switch (getMovie().getPriceCode()) {
